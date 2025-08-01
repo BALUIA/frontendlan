b9ec9d75ecf0e0e9008ca919b99b281e11cff9e7
Baluia <baluiaperua@gmail.com>
2025-08-01 12:54:21 -0500

    fix: boton

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -14,6 +14,7 @@
                 prepend-icon="mdi-account"
                 type="text"
                 required
+                :disabled="isLoading"
               ></v-text-field>
               <v-text-field
                 label="Password"
@@ -21,11 +22,19 @@
                 prepend-icon="mdi-lock"
                 type="password"
                 required
+                :disabled="isLoading"
               ></v-text-field>
+              
               <v-alert v-if="errorMessage" type="error" dense>{{ errorMessage }}</v-alert>
+
+              <div v-if="isLoading" class="text-center my-4">
+                <v-progress-circular indeterminate color="primary"></v-progress-circular>
+                <p class="mt-2">Iniciando el servidor... Esto puede tardar hasta 2 minutos.</p>
+              </div>
+
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="primary" type="submit">Login</v-btn>
+                <v-btn color="primary" type="submit" :disabled="isLoading">Login</v-btn>
               </v-card-actions>
             </v-form>
           </v-card-text>
@@ -43,15 +52,20 @@ import { useRouter } from 'vue-router';
 const username = ref('');
 const password = ref('');
 const errorMessage = ref('');
+const isLoading = ref(false);
 const authStore = useAuthStore();
 const router = useRouter();
 
 const handleLogin = async () => {
+  isLoading.value = true;
+  errorMessage.value = '';
   try {
     await authStore.login(username.value, password.value);
     router.push('/');
   } catch (error) {
     errorMessage.value = error.message;
+  } finally {
+    isLoading.value = false;
   }
 };
 </script>
